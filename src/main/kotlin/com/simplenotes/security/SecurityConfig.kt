e7ddcce272276334af3319b21686c7291d805a9e
Vinicius Altrao <viniciusaltrao@gmail.com>
2025-04-07 21:18:09 +0200

    feat: add security components and utilities
    - Security configuration
    - User details service implementation
    - Utility helper class
    - Security component tests

---
+++ src/main/kotlin/com/simplenotes/security/SecurityConfig.kt
@@ -0,0 +1,58 @@
+package com.simplenotes.security
+
+import com.simplenotes.filter.JwtAuthorizationFilter
+import com.simplenotes.filter.RateLimiterFilter
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.authentication.AuthenticationProvider
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.web.DefaultSecurityFilterChain
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+
+@Configuration
+open class SecurityConfig {
+    @Bean
+    open fun authenticationManager(config: AuthenticationConfiguration): AuthenticationManager = config.authenticationManager
+
+    @Bean
+    open fun authenticationProvider(userDetailsService: UserDetailsService): AuthenticationProvider {
+        return DaoAuthenticationProvider().apply {
+            setUserDetailsService(userDetailsService)
+            setPasswordEncoder(encoder())
+        }
+    }
+
+    @Bean
+    open fun securityFilterChain(
+        http: HttpSecurity,
+        jwtAuthenticationFilter: JwtAuthorizationFilter,
+        authenticationProvider: AuthenticationProvider,
+        rateLimiterFilter: RateLimiterFilter
+    ): DefaultSecurityFilterChain {
+        http.csrf { it.disable() }
+            .authorizeHttpRequests {
+                it.requestMatchers("/api/auth", "/api/auth/refresh", "/api/auth/register", "/error")
+                    .permitAll()
+                    .anyRequest()
+                    .fullyAuthenticated()
+            }
+            .sessionManagement {
+                it.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            }
+            .authenticationProvider(authenticationProvider)
+            .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter::class.java)
+            .addFilterBefore(rateLimiterFilter, JwtAuthorizationFilter::class.java)
+
+        return http.build()
+    }
+
+    @Bean
+    open fun encoder(): PasswordEncoder = BCryptPasswordEncoder()
+}
---
+++ src/main/kotlin/com/simplenotes/security/UserDetailService.kt
@@ -0,0 +1,66 @@
+package com.simplenotes.security
+
+import com.simplenotes.exception.RegisterException
+import com.simplenotes.model.AuthenticationRequest
+import com.simplenotes.model.User
+import com.simplenotes.repository.UserRepository
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.core.userdetails.UsernameNotFoundException
+import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.stereotype.Service
+
+@Service
+class UserDetailsService(
+    private val userRepository: UserRepository,
+    private val passwordEncoder: PasswordEncoder
+) : UserDetailsService {
+    val emailRegex = Regex("^[\\w-.]+@([\\w-]+\\.)+[\\w-]{2,4}$")
+    val passwordRegex = Regex("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{8,}$")
+
+    fun register(request: AuthenticationRequest): User {
+        checkUsernameAndPassword(request)
+
+        val existingUser = userRepository.findByEmail(request.username)
+
+        if (existingUser != null) {
+            throw RegisterException("User already exists")
+        }
+
+        return userRepository.createUser(request.username, passwordEncoder.encode(request.password))
+    }
+
+    override fun loadUserByUsername(username: String): UserDetails {
+        val persistedUser = userRepository.findByEmail(username) ?: throw UsernameNotFoundException("User not found")
+        return User(persistedUser.id, persistedUser.email, persistedUser.pass, persistedUser.role)
+    }
+
+    private fun checkUsernameAndPassword(request: AuthenticationRequest) {
+        if (!isValidPassword(request.password)) {
+            throw RegisterException("Password does not meet the required criteria.")
+        }
+
+        if (!isValidUsername(request.username)) {
+            throw RegisterException("Invalid email format.")
+        }
+
+    }
+
+    private fun isValidUsername(username: String): Boolean {
+        return username.matches(emailRegex)
+    }
+
+    /**
+     * Validates whether the given password meets the predefined criteria:
+     * - Minimum 8 characters
+     * - Contains at least one digit
+     * - Contains at least one uppercase letter
+     * - Contains at least one lowercase letter
+     *
+     * @param password the password string to be validated
+     * @return true if the password meets the criteria, false otherwise
+     */
+    private fun isValidPassword(password: String): Boolean {
+        return password.matches(passwordRegex)
+    }
+}
