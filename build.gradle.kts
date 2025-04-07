c5c328f9d8392dfe7e65bbdd84fc841e19dc6098
Vinicius Altrao <viniciusaltrao@gmail.com>
2025-04-07 21:21:06 +0200

    build: add project configuration files
    - Gradle build configuration
    - Application entry point
    - Git ignore rules

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,11 +2,10 @@ plugins {
     kotlin("jvm") version "2.1.10"
 }
 
-group = "com.securenotes"
+group = "com.simplenotes"
 version = "1.0-SNAPSHOT"
 
 val springVersion = "3.4.4"
-val springSecurityVersion = "6.4.4"
 val jjwtVersion = "0.12.6"
 
 repositories {
@@ -15,18 +14,26 @@ repositories {
 
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib:2.1.10")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:1.10.1")
     implementation("org.springframework.boot:spring-boot-starter-security:$springVersion")
     implementation("org.springframework.boot:spring-boot-starter-web:$springVersion")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.17.2")
     implementation("org.jetbrains.kotlin:kotlin-reflect:2.1.20")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa:$springVersion")
     implementation("io.jsonwebtoken:jjwt-api:$jjwtVersion")
     implementation("io.jsonwebtoken:jjwt-impl:$jjwtVersion")
+    implementation("com.github.ben-manes.caffeine:caffeine:3.2.0")
+    implementation("com.bucket4j:bucket4j_jdk17-core:8.14.0")
+
     runtimeOnly("io.jsonwebtoken:jjwt-jackson:$jjwtVersion")
+    runtimeOnly("org.postgresql:postgresql:42.7.5")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test:$springVersion")
-    testImplementation("org.springframework.security:spring-security-test:$springSecurityVersion")
+    testImplementation("org.springframework.security:spring-security-test:6.4.4")
     testImplementation("org.mockito.kotlin:mockito-kotlin:5.4.0")
-//    testImplementation("io.mockk:mockk:1.13.17")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.3")
+    testImplementation("com.h2database:h2:2.3.232")
     testImplementation(kotlin("test"))
 }
 
@@ -36,4 +43,8 @@ tasks.test {
 
 kotlin {
     jvmToolchain(17)
+
+    compilerOptions {
+        freeCompilerArgs.add("-Xjvm-default=all")
+    }
 }
--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -1,11 +1,13 @@
-package com.securenotes
+package com.simplenotes
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableScheduling
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 
 
 @EnableWebSecurity
+@EnableScheduling
 @SpringBootApplication
 open class Application
 
